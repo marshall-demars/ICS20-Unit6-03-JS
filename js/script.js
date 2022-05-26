b21136b366bd27cf0b0aa3803a417dde60718392
Marshall Demars <99354910+marshall-demars@users.noreply.github.com>
2022-05-26 16:40:13 +0000

    2 files

--- js/script.js
+++ js/script.js
@@ -24,12 +24,13 @@ const getWeather = async (URLAddress) => {
     const jsonData = await result.json()
     console.log(jsonData.main.temp)
     const temperature = jsonData.main.temp - 273.15
-    console.log = (jsonData.weather.con)
-    const symbol = jsonData.weather.icon
-    document.getElementById("weather").innerHTML = "The current weather in Ottawa is " + temperature.toFixed(0) + "℃ " + symbol
-    } catch (err) {
-      console.log(err)
+    const symbol = jsonData.weather[0].icon
+    const description = jsonData.weather[0].description
+    document.getElementById("weather").innerHTML = "The current weather is " + temperature.toFixed(0) + "°C" + "<img src='https://openweathermap.org/img/wn/" + symbol + "@2x.png' alt='Weather Icon' width='15%'>" + "with " + description
+  } catch (err) {
+    console.log(err)
+    document.getElementById("weather").innerHTML = "Error fetching current weather."
   }
-}  
+}
 
     getWeather("https://api.openweathermap.org/data/2.5/weather?lat=45.4211435&lon=-75.6900574&appid=fe1d80e1e103cff8c6afd190cad23fa5")
